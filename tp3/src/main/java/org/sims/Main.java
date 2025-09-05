17410bb2c4bd856531bb564efd38f6da43223ab3
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-05 00:28:15 -0300

    Example simulator writer

--- tp3/src/main/java/org/sims/Main.java
+++ tp3/src/main/java/org/sims/Main.java
@@ -3,10 +3,10 @@ package org.sims;
 import org.sims.models.Particle;
 import org.sims.models.Wall;
 
-import java.io.IOException;
+import java.util.concurrent.Executors;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         final var walls = Wall.generate(0.06);
         final var particles = Particle.generateInitialState(20, 0.3, 0.005);
 
@@ -23,6 +23,29 @@ public class Main {
         }
 
         environment_writer.close();
+
+        Resources.preparePath("steps");
+
+        final var sim = new Simulation(100, particles, walls);
+        try (final var engine = sim.engine(); final var writer = Executors.newSingleThreadExecutor()) {
+            for (final var step : engine) {
+                final var i = step.i();
+
+                if (i % 5 == 0) {
+                    writer.submit(() -> {
+                        final var filename = "step_%03d.txt".formatted(i / 5);
+
+                        try (final var step_writer = Resources.writer("steps", filename)) {
+                            for (final var p : step.particles()) {
+                                step_writer.write(p.toString() + "\n");
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+                }
+            }
+        }
     }
 
     // TODO wall generator that checks closed polygon
