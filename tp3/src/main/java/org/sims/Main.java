474019e3230516e9ff788f7e7cf980179163974b
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-05 11:54:06 -0300

    Bring back the try with resources
    :]

--- tp3/src/main/java/org/sims/Main.java
+++ tp3/src/main/java/org/sims/Main.java
@@ -10,19 +10,11 @@ public class Main {
         final var walls = Wall.generate(0.06);
         final var particles = Particle.generateInitialState(20, 0.3, 0.005);
 
-        final var environment_writer = Resources.writer("particles.txt");
-
-        environment_writer.write("Walls:\n");
-        for (final var w : walls) {
-            environment_writer.write(w.toString() + "\n");
+        try (final var writer = Resources.writer("walls.txt")) {
+            for (final var w : walls) {
+                writer.write(w.toString() + "\n");
+            }
         }
-
-        environment_writer.write("Particles:\n");
-        for (final var p : particles) {
-            environment_writer.write(p.toString() + "\n");
-        }
-
-        environment_writer.close();
 
         Resources.preparePath("steps");
 
