0583b60f89eb6244ab2b83afad7889230bcf247b
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-09 14:08:49 -0300

    Dead code

--- tp3/src/main/java/org/sims/Engine.java
+++ tp3/src/main/java/org/sims/Engine.java
@@ -33,25 +33,15 @@ public record Engine(Simulation simulation) implements Iterable<Step> {
             @Override
             public Step next() {
                 final var events = new LinkedList<Event>();
-
-                while (!queue.isEmpty() && !queue.peek().valid(current)) {
-                    queue.poll();
-                }
-
                 final var event = queue.poll();
 
                 moveAll(event.time() - time /* Time-Skip */);
                 time = event.time();
 
                 Particle.collide(event.p(), event.c());
-
                 events.add(event.clone());
 
                 calculateEvents(event.involved(), time, queue);
-
-                if (queue.isEmpty()) {
-                    throw new IllegalStateException("No more events to process");
-                }
 
                 return new Step(++current, Particle.deepCopy(particles), List.copyOf(events));
             }
