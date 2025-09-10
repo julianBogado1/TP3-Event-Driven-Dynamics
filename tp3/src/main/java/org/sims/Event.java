e1aa5b0eae13567ac398a60ceab9cb3106803497
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-09 21:40:05 -0300

    Revert "Clean up Event"
    This reverts commit a3b701f1852990b079c063381819b122553efdf6.

--- tp3/src/main/java/org/sims/Event.java
+++ tp3/src/main/java/org/sims/Event.java
@@ -5,8 +5,31 @@ import java.util.stream.Stream;
 import org.sims.models.Collideable;
 import org.sims.models.Particle;
 
-public record Event(Particle p, Collideable c, double time)
+public record Event(Particle p, Collideable c, double time, long etag)
         implements Comparable<Event>, Cloneable {
+    public Event(Particle p, Collideable c, double time) {
+        this(p, c, time, Event.etag(p, c));
+    }
+
+    /**
+     * Generates an event tag based on the number of events each particle has
+     *
+     * @param p A particle
+     * @param c A collideable
+     * @return The event tag
+     */
+    private static long etag(final Particle p, final Collideable c) {
+        var etag = p.getEvents();
+        if (c instanceof final Particle p2) {
+            etag += p2.getEvents();
+        }
+        return etag;
+    }
+
+    public boolean valid(double currentTime) {
+        return this.time >= currentTime && this.etag == Event.etag(p, c);
+    }
+
     public Stream<Particle> involved() {
         if (c instanceof final Particle p2) {
             return Stream.of(p, p2);
@@ -22,6 +45,6 @@ public record Event(Particle p, Collideable c, double time)
 
     @Override
     public Event clone() {
-        return new Event(p.clone(), c.clone(), time);
+        return new Event(p.clone(), c.clone(), time, etag);
     }
 }
