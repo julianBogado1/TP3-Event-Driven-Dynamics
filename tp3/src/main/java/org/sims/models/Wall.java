2063dc15fc3924252a8bb49be70f0eea7616a0ab
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-11 06:52:19 -0300

    Fix Wall collisionTime once and for all
    Copy pasted + translated from my branch 😄

--- tp3/src/main/java/org/sims/models/Wall.java
+++ tp3/src/main/java/org/sims/models/Wall.java
@@ -2,12 +2,15 @@ package org.sims.models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class Wall {
+    private final Orientation orientation;
     private final Vector vertex1;
     private final Vector vertex2;
 
-    public Wall(final Vector v1, final Vector v2) {
+    public Wall(final Orientation orientation, final Vector v1, final Vector v2) {
+        this.orientation = orientation;
         this.vertex1 = v1;
         this.vertex2 = v2;
     }
@@ -15,6 +18,7 @@ public class Wall {
     public Vector getVertex1() {
         return vertex1;
     }
+
     public Vector getVertex2() {
         return vertex2;
     }
@@ -29,14 +33,14 @@ public class Wall {
         final double lilCorner = (0.09 - L) / 2.0;
         final List<Wall> walls = new ArrayList<>(8);
 
-        walls.add(new Wall(new Vector(0, 0), new Vector(0.09, 0)));
-        walls.add(new Wall(new Vector(0.09, 0), new Vector(0.09, lilCorner)));
-        walls.add(new Wall(new Vector(0.09, lilCorner), new Vector(0.18, lilCorner)));
-        walls.add(new Wall(new Vector(0.18, lilCorner), new Vector(0.18, lilCorner + L)));
-        walls.add(new Wall(new Vector(0.18, lilCorner + L), new Vector(0.09, lilCorner + L)));
-        walls.add(new Wall(new Vector(0.09, lilCorner + L), new Vector(0.09, 0.09)));
-        walls.add(new Wall(new Vector(0.09, 0.09), new Vector(0, 0.09)));
-        walls.add(new Wall(new Vector(0, 0.09), new Vector(0, 0)));
+        walls.add(new Wall(Orientation.HORIZONTAL, new Vector(0, 0), new Vector(0.09, 0)));
+        walls.add(new Wall(Orientation.VERTICAL, new Vector(0.09, 0), new Vector(0.09, lilCorner)));
+        walls.add(new Wall(Orientation.HORIZONTAL, new Vector(0.09, lilCorner), new Vector(0.18, lilCorner)));
+        walls.add(new Wall(Orientation.VERTICAL, new Vector(0.18, lilCorner), new Vector(0.18, lilCorner + L)));
+        walls.add(new Wall(Orientation.HORIZONTAL, new Vector(0.18, lilCorner + L), new Vector(0.09, lilCorner + L)));
+        walls.add(new Wall(Orientation.VERTICAL, new Vector(0.09, lilCorner + L), new Vector(0.09, 0.09)));
+        walls.add(new Wall(Orientation.HORIZONTAL, new Vector(0.09, 0.09), new Vector(0, 0.09)));
+        walls.add(new Wall(Orientation.VERTICAL, new Vector(0, 0.09), new Vector(0, 0)));
 
         return walls;
     }
@@ -44,76 +48,105 @@ public class Wall {
     /**
      * Compute timo of the wall with the particle
      *
+     * @apiNote This might look like magic, but it's just
+     *          taking advantage of the fact that the logic is the
+     *          same for vertical and horizontal walls, just
+     *          swapping x and y
+     *
+     * @throws IllegalArgumentException if wall orientation is not VERTICAL or
+     *                                  HORIZONTAL
      * @param p particle to check collision with
      * @return collision time
      */
     public double collisionTime(final Particle p) {
-        final Vector pVelocity;
-        final Vector pCurrentPos;
+        if (orientation.equals(Orientation.ANY)) {
+            throw new IllegalArgumentException("Unsupported Wall orientation");
+        }
 
-        if (Math.abs(vertex1.getX() - vertex2.getX()) < 0.00001) { // vertical wall
-            final double wallX = vertex1.getX();
+        // Get the constant coordinate of the wall (x for vertical, y for horizontal)
+        final var wall = orientation.constant(vertex1);
+        // Get the variable coordinates of the wall (y for vertical, x for horizontal)
+        final var cvel = orientation.constant(p.getVelocity());
+        final var cpos = orientation.constant(p.getPosition());
 
-            pVelocity = p.getVelocity();
-            pCurrentPos = p.getPosition(); // Use current position, not initial
+        // Check if the particle is moving towards the wall
+        final var bottom_left = cpos < wall && cvel > 0;
+        final var top_right = wall < cpos && cvel < 0;
+        // These variables read as "the particle comes from a or b"
 
-            final double toReturn;
-
-            if (wallX > pCurrentPos.getX() && pVelocity.getX() > 0) {
-                toReturn = (wallX - p.getRadius() - pCurrentPos.getX()) / pVelocity.getX();
-
-                if (toReturn < 0) {
-                    return Double.POSITIVE_INFINITY;
-                } else if(pCurrentPos.getY() + pVelocity.getY() * toReturn > Math.max(vertex1.getY(), vertex2.getY())
-                        || pCurrentPos.getY() + pVelocity.getY() * toReturn < Math.min(vertex1.getY(), vertex2.getY())) {
-                    
-                    return toReturn;
-                }
-            } else if (wallX < pCurrentPos.getX() && pVelocity.getX() < 0) {
-                toReturn = (wallX + p.getRadius() - pCurrentPos.getX()) / pVelocity.getX();
-
-                if (toReturn < 0) {
-                    return Double.POSITIVE_INFINITY;
-                } else if(pCurrentPos.getY() + pVelocity.getY() * toReturn > Math.max(vertex1.getY(), vertex2.getY())
-                        || pCurrentPos.getY() + pVelocity.getY() * toReturn < Math.min(vertex1.getY(), vertex2.getY())) {
-                    
-                    return toReturn;
-                }
-            }
-
-            return Double.POSITIVE_INFINITY;
-        } else if (Math.abs(vertex1.getY() - vertex2.getY()) < 0.00001) { // horizontal wall
-            final double wallY = vertex1.getY();
-
-            pVelocity = p.getVelocity();
-            pCurrentPos = p.getPosition(); // Use current position, not initial
-
-            if (wallY > pCurrentPos.getY() && pVelocity.getY() > 0) {
-                final double toReturn = (wallY - p.getRadius() - pCurrentPos.getY()) / pVelocity.getY();
-
-                if (toReturn < 0) {
-                    return Double.POSITIVE_INFINITY;
-                } else if(pCurrentPos.getX() + pVelocity.getX() * toReturn > Math.max(vertex1.getX(), vertex2.getX())
-                        || pCurrentPos.getX() + pVelocity.getX() * toReturn < Math.min(vertex1.getX(), vertex2.getX())) {
-                    
-                    return toReturn;
-                }
-            } else if (wallY < pCurrentPos.getY() && pVelocity.getY() < 0) {
-                final double toReturn = (wallY + p.getRadius() - pCurrentPos.getY()) / pVelocity.getY();
-
-                if (toReturn < 0) {
-                    return Double.POSITIVE_INFINITY;
-                } else if(pCurrentPos.getX() + pVelocity.getX() * toReturn > Math.max(vertex1.getX(), vertex2.getX())
-                        || pCurrentPos.getX() + pVelocity.getX() * toReturn < Math.min(vertex1.getX(), vertex2.getX())) {
-                    
-                    return toReturn;
-                }
-            }
-
+        if (!bottom_left && !top_right) {
             return Double.POSITIVE_INFINITY;
         }
 
-        return Double.POSITIVE_INFINITY;
+        // If the particle comes from bottom/left, the radius is subtracted,
+        // otherwise added
+        final var r_effect = (bottom_left ? -1 : +1) * p.getRadius();
+        final var time = (wall + r_effect - cpos) / cvel;
+
+        // Never happens, top_left and bottom_right ensures the division is positive
+        if (time < 0) {
+            return Double.POSITIVE_INFINITY;
+        }
+
+        // Get the future position of the particle at collision time
+        final var future = Particle.move(p, time);
+
+        // If the particle in the future is not between the wall's length
+        if (!between(orientation.variable(future), orientation.variable(vertex1), orientation.variable(vertex2))) {
+            return Double.POSITIVE_INFINITY;
+        }
+
+        // Finally, peace
+        return time;
+    }
+
+    public static enum Orientation {
+        VERTICAL(Vector::getX, Vector::getY), HORIZONTAL(Vector::getY, Vector::getX), ANY;
+
+        private final Function<Vector, Double> constant;
+        private final Function<Vector, Double> variable;
+
+        Orientation(final Function<Vector, Double> constant, final Function<Vector, Double> variable) {
+            this.constant = constant;
+            this.variable = variable;
+        }
+
+        Orientation() {
+            this(_ -> null, _ -> null);
+        }
+
+        /**
+         * Get the constant coordinate of a vector according to the orientation type
+         *
+         * @param v vector to get coordinate from
+         * @return constant value
+         */
+        public double constant(Vector v) {
+            return constant.apply(v);
+        }
+
+        /**
+         * Get the variable coordinate of a vector according to the orientation type
+         *
+         * @param v vector to get coordinate from
+         * @return variable value
+         */
+        public double variable(Vector v) {
+            return variable.apply(v);
+        }
+    }
+
+    /**
+     * Checks if a value is between two other values, inclusive,
+     * regardless of order
+     *
+     * @param val value to check
+     * @param a   first bound
+     * @param b   second bound
+     * @return true if val is between a and b
+     */
+    private static final boolean between(double val, double a, double b) {
+        return Math.min(a, b) <= val && val <= Math.max(a, b);
     }
 
     @Override
