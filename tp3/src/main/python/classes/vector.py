05e61209ea48054cec71189ad6e124b49b5bcf86
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-12 11:36:56 -0300

    Add vector methods to Python

--- tp3/src/main/python/classes/vector.py
+++ tp3/src/main/python/classes/vector.py
@@ -7,3 +7,26 @@ class Vector:
 
     def tuple(self) -> tuple[float, float]:
         return (self.x, self.y)
+
+    def __add__(self, other: 'Vector') -> 'Vector':
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: 'Vector') -> 'Vector':
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: float) -> 'Vector':
+        return Vector(self.x * other, self.y * other)
+
+    def __div__(self, other: float) -> 'Vector':
+        return Vector(self.x / other, self.y / other)
+
+    def dot(self, other: 'Vector') -> float:
+        return self.x * other.x + self.y * other.y
+
+    def norm(self) -> float:
+        return (self.x**2 + self.y**2) ** 0.5
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Vector):
+            return NotImplemented
+        return self.x == other.x and self.y == other.y
