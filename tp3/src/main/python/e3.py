607e57599a4c9dc09f3f806bd3b43c5939f6c92b
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-09-18 05:14:29 -0300

    Create e3.py

---
+++ tp3/src/main/python/e3.py
@@ -0,0 +1,30 @@
+import sys
+
+import numpy as np
+
+from errors import fitter, origin, plot, FIT, LINEAR
+from pa import main as pressure_math, study
+
+def main(F: FIT):
+    STUDY = study('areant')
+
+    if STUDY is None:
+        sys.exit(1)
+
+    RESULTS = pressure_math(STUDY[0])
+    X = np.array(RESULTS[0])
+    Y = np.array(RESULTS[1])
+
+    C, E, MIN_X, MIN_Y = fitter(X, Y, F, 0, 0.029)
+    print(f'C: {MIN_X:.4f}')
+
+    plot(C, E, MIN_X, MIN_Y)
+
+if __name__ == "__main__":
+    P1 = (float(sys.argv[1]), float(sys.argv[2]))
+    P2 = (float(sys.argv[3]), float(sys.argv[4]))
+
+    B = origin(P1, P2)
+    print(f'B: {B:.4f}')
+
+    main(LINEAR(B))
